b2a56b33cb21b54e3efdf0f8f1ea495f49eab5bf
Gabriel <GasparHome@email.com>
2019-10-30 12:04:15 -0300

    Removendo dados sensiveis

--- src/services/APIData.js
+++ src/services/APIData.js
@@ -1,13 +1,13 @@
 // Insert Here your google geolocation API's data
 const googleAppData = {
-    API_KEY:"AIzaSyA_1ASYutUfJL6QY2Ef_LU51jsJZkIbc-w"
+    API_KEY:"API_KEY"
 }
 
 // Insert here your GItHub client app's data
 const GitAppData = {
-    clientId: '4d5e5470c4da4dd71d4f',
-    clientSecret: 'b03e3fae2d8c669f7d4de9109edc9fabec05371a',
-    redirectUrl: 'io.devfinder://callback',
+    clientId: 'CLIENT_ID',
+    clientSecret: 'CLIENT_SECRET',
+    redirectUrl: 'CALLBACK_URL',
     scopes: ['public_repo', 'read:user'],
 }
 
