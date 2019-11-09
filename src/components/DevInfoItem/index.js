8d48d25e7c2526d831dfe4f6bbceb8eb2fd0c4d7
Gabriel <GasparHome@email.com>
2019-11-09 20:10:44 -0300

    DevInfoItem Ok

--- src/components/DevInfoItem/index.js
+++ src/components/DevInfoItem/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {View,Text} from 'react-native'
 
 import colors from '../../styles/colors';
-import styles from './styles';
+
+import {InfoBox,LabelBox,ValueBox,Label,Value} from './styles'
 
 const devInfoItem = (props) => {
     return(
-        <View style={styles.devInfoContainer}>
-            <View style={{flex:1,backgroundColor:colors.themeColor, borderRadius:10}}>
-                <Text style={{alignSelf:'center', padding:5, color:'white', fontWeight:'bold'}}>{props.label}</Text>
-            </View>
-            <View style={{flex:2}}>
-                <Text style={styles.detailValue}>{props.value}</Text>
-            </View> 
-        </View>
+        <InfoBox>
+            <LabelBox theme={colors.themeColor}>
+                <Label>{props.label}</Label>
+            </LabelBox>
+            <ValueBox>
+                <Value>{props.value}</Value>
+            </ValueBox> 
+        </InfoBox>
     );
 }
 
--- src/components/DevInfoItem/styles.js
+++ src/components/DevInfoItem/styles.js
@@ -1,18 +1,33 @@
-import {StyleSheet} from 'react-native';
 import styled from 'styled-components';
 
-const styles = StyleSheet.create({
-    devInfoContainer:{
-        flexDirection:'row',
-        alignItems:'center',
-        justifyContent:'flex-start',
-        margin:5
-    },
-    detailValue:{
-        alignSelf:'flex-start',
-        fontWeight:'bold',
-        padding:5        
-    }
-});
+const InfoBox = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    margin: 5px;
+`;
 
-export default styles;
+const LabelBox = styled.View`
+    flex:1;
+    background-color: ${props => props.theme}
+    border-radius: 10px;
+`;
+
+const ValueBox = styled.View`
+    flex:2;
+`;
+
+const Label = styled.Text`
+    align-self: center;
+    padding: 5px;
+    color: white;
+    font-weight: bold;
+`;
+
+const Value = styled.Text`
+    align-self: flex-start;
+    font-weight: bold;
+    padding: 5px;
+`;
+
+export {InfoBox,LabelBox,ValueBox,Label,Value};
