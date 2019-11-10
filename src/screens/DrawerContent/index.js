b6eab42ab05079dced87cbcdc51e76088da00861
Gabriel <GasparHome@email.com>
2019-11-10 17:36:17 -0300

    DrawerContent Ok

--- src/screens/DrawerContent/index.js
+++ src/screens/DrawerContent/index.js
@@ -1,43 +1,45 @@
 import React from 'react';
-import {View, Text, Image, TouchableOpacity, ToastAndroid} from 'react-native';
+import {ToastAndroid} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-
 import colors from '../../styles/colors';
-import styles from './styles';
+import {
+    headerContainer,
+    bodyContainer,
+    Container,
+    Header,
+    Logo,
+    AppName,
+    AppBtn,
+    TextBtn  
+} from './styles';
 import GitHubApi from '../../services/GitHubApi';
-
 import {setToken} from '../../actions/actions';
 import {connect} from 'react-redux'
 
 const drawerNavigator = (props) => {
 
     return(
-        <View style={styles.drawerContainer}>
+        <Container>
             <LinearGradient 
                 colors={colors.linearGradientColors}
-                style={styles.headerContainer}
+                style={headerContainer}
             >
-                <View style={styles.appHeader}>
-                    <Image 
-                            style={styles.appLogo}
-                            source={require('../../assets/images/alvo.png')}/>
-                    <Text style={styles.headerText}>DevFinder</Text>
-                </View>       
+                <Header>
+                    <Logo source={require('../../assets/images/alvo.png')}/>
+                    <AppName>DevFinder</AppName>
+                </Header>       
             </LinearGradient>
             <LinearGradient 
                 start={{x: 0, y: 0}}
                 end={{x: 1, y: 0}}
                 colors={colors.secondaryGradient}
-                style={styles.bodyContainer}
+                style={bodyContainer}
             >
-                <TouchableOpacity
-                    style={styles.btn}
-                    onPress={()=> props.navigation.closeDrawer()}>
-                        <Text style={styles.textBtn}>App</Text>
-                </TouchableOpacity>
+                <AppBtn onPress={()=> props.navigation.closeDrawer()}>
+                    <TextBtn>App</TextBtn>
+                </AppBtn>
 
-                <TouchableOpacity
-                    style={styles.btn}
+                <AppBtn
                     onPress={async ()=>{
                         console.log('Deslogar do app!');
                         ToastAndroid.show(
@@ -46,12 +48,12 @@ const drawerNavigator = (props) => {
                         );
                         props.dispatch(setToken(''));
                         await GitHubApi.deleteToken();
-                        props.navigation.navigate('AboutApp')}}>
-                        <Text style={styles.textBtn}>Login out</Text>
-                </TouchableOpacity>
-
+                        props.navigation.navigate('AboutApp')}}
+                >
+                        <TextBtn>Login out</TextBtn>
+                </AppBtn>
             </LinearGradient>
-        </View>
+        </Container>
     );
 }
 
