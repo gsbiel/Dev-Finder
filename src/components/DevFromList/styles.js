28ccffaa487aa2f97e3f0693259d5128733f2ca1
Gabriel <GasparHome@email.com>
2019-11-12 21:52:29 -0300

    normalizando telas

--- src/components/DevFromList/styles.js
+++ src/components/DevFromList/styles.js
@@ -1,31 +1,31 @@
 import styled from 'styled-components';
+import normalize from 'react-native-normalize';
 
 const DevContainer =  styled.TouchableOpacity`
     background-color: #fff;
-    padding: 10px;
-    margin: 10px;
-    border-radius: 5px;
+    padding: ${normalize(10)}px;
+    margin: ${normalize(10)}px;
+    border-radius: ${normalize(5)}px;
     flex-direction: row;
     align-items: center;
 `;
 
 const DevImage = styled.Image`
-    width: 75px;
-    height: 75px;
-    border-radius: 100px;
+    width: ${normalize(75)};;
+    height: ${normalize(75)};;
+    border-radius: ${normalize(100)};;
 `;
 
 const InfoBox = styled.View`
     flex-direction: column;
-    padding-left: 10px;
-    padding-right: 10px;
+    padding-left: ${normalize(10)};;
+    padding-right: ${normalize(10)};;
 `;
 
 const InfoItem = styled.Text`
-    font-size: 15px;
+    font-size: ${normalize(15)};;
     font-weight: 600; 
-    margin-bottom: 15px;
-
+    margin-bottom: ${normalize(15)};;
 `;
 
 export {DevContainer,DevImage,InfoBox,InfoItem};
